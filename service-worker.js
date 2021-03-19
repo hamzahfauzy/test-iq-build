537104031efb84075695522cadfc1c535a4c3056
hamzahfauzy <hamzahfauzy97@gmail.com>
2021-03-19 11:52:31 +0700

    remove for

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27b750102dbe15324021fcd2f85c96f0.js"
+  "/precache-manifest.426fa0a9f8c0752239c3f9941d4d1434.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tmc-exam"});
