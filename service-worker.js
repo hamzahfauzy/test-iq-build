5fab7dcfd12b62ed7f3f5e60ffef8f61521aaf52
hamzahfauzy <hamzahfauzy97@gmail.com>
2021-03-20 16:40:03 +0700

    login bug final

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d744fcee4be138361d0fc47d183192ed.js"
+  "/precache-manifest.22926513198e03608f98715657560f1e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tmc-exam"});
